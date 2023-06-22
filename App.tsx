11976129a43c17f22e1b9bbec737e8c37ef47c6b
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2023-06-22 17:10:27 +0530

    chore: :wrench: add basic google-fit

--- App.tsx
+++ App.tsx
@@ -6,12 +6,14 @@ import {
   Button,
   StyleSheet,
   Text,
+  ToastAndroid,
   View,
 } from "react-native";
 import GoogleFit, { Scopes } from "react-native-google-fit";
 
 export default function App() {
   const [isConnected, setIsConnected] = useState<boolean>(false);
+  const [steps, setSteps] = useState<number>(0);
 
   const checkOnLoad = async () => {
     const res = await GoogleFit.authorize({
@@ -29,6 +31,11 @@ export default function App() {
     }
 
     setIsConnected(true);
+
+    const data = await GoogleFit.getDailySteps(new Date());
+
+    console.log(data[2].steps);
+    console.log(data);
   };
 
   const onDisconnectClick = () => {
@@ -40,9 +47,27 @@ export default function App() {
     checkOnLoad();
   }, []);
 
+  useEffect(() => {
+    setInterval(async () => {
+      ToastAndroid.show("HERE!!", 100);
+      if (isConnected) {
+        const data = await GoogleFit.getDailySteps(new Date());
+        console.log(data);
+
+        if (data.length < 1) return;
+        ToastAndroid.show(`${data[data.length - 1]?.steps[0]?.value}`, 1000);
+        console.log(data[data.length - 1].steps[0]?.value);
+        if (data[data.length - 1]?.steps[0]?.value) {
+          setSteps(data[data.length - 1]?.steps[0]?.value);
+        }
+      }
+    }, 10000);
+  }, [isConnected]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Steps:</Text>
+      <Text style={styles.stepText}>{steps}</Text>
       {isConnected && <Button title="Disconnect" onPress={onDisconnectClick} />}
       <StatusBar style="dark" />
     </View>
@@ -55,5 +80,9 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  stepText: {
+    fontSize: 25,
+    marginVertical: 10,
   },
 });
