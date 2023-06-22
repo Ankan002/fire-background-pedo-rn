7a8b2b776359a28687a0b94a2d9eb11fe3f96cf8
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2023-06-22 15:13:35 +0530

    chore: :wrench: add basic google-fit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useEffect, useState } from "react";
+import {
+  Alert,
+  BackHandler,
+  Button,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import GoogleFit, { Scopes } from "react-native-google-fit";
 
 export default function App() {
+  const [isConnected, setIsConnected] = useState<boolean>(false);
+
+  const checkOnLoad = async () => {
+    const res = await GoogleFit.authorize({
+      scopes: [
+        Scopes.FITNESS_ACTIVITY_READ,
+        Scopes.FITNESS_ACTIVITY_WRITE,
+        Scopes.FITNESS_BODY_READ,
+        Scopes.FITNESS_BODY_WRITE,
+      ],
+    });
+
+    if (!res.success) {
+      Alert.alert("Not Auth");
+      BackHandler.exitApp();
+    }
+
+    setIsConnected(true);
+  };
+
+  const onDisconnectClick = () => {
+    GoogleFit.disconnect();
+    setIsConnected(false);
+  };
+
+  useEffect(() => {
+    checkOnLoad();
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {isConnected && <Button title="Disconnect" onPress={onDisconnectClick} />}
+      <StatusBar style="dark" />
     </View>
   );
 }
@@ -13,8 +52,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
